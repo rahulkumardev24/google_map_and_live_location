27226a62d02d9a642521a2d55c1d6517b099f01c
rahul <rkrahulroy151617@gmail.com>
2025-01-20 00:14:18 +0530

    Google Map

---
+++ android/app/src/main/kotlin/com/example/google_map_and_live_location/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.google_map_and_live_location
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screen/google_map_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class GoogleMapScreen extends StatefulWidget {
+  const GoogleMapScreen({super.key});
+
+  @override
+  State<GoogleMapScreen> createState() => _GoogleMapScreenState();
+}
+
+class _GoogleMapScreenState extends State<GoogleMapScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Google Map"),),
+      /// here we show google map
+      body: const GoogleMap(
+        mapType: MapType.satellite,
+
+          zoomGesturesEnabled: true,
+          indoorViewEnabled: true,
+          initialCameraPosition: CameraPosition(
+        /// latitude abd longitude
+          target: LatLng(28.612894, 77.229446) ,
+        zoom: 18 ,
+        tilt: 40.65 ,
+      ) ),
+    ) ;
+  }
+}
+
+/// in next video track live location
